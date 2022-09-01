637afbabf9226ab7b13c9aecd4a7c028b6ba5c17
oreki <912550157@qq.com>
2022-09-01 16:45:07 +0800

    goods_api-logger

--- web/goods_api/api/brands/brands.go
+++ web/goods_api/api/brands/brands.go
@@ -5,3 +5,244 @@
 **/
 
 package brands
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"goods_api/api"
+	"goods_api/global"
+	in "goods_api/interface"
+	"goods_api/struct/form"
+	"net/http"
+	"strconv"
+)
+
+func BrandList(ctx *gin.Context) {
+	pn := ctx.DefaultQuery("pn", "0")
+	pnInt, _ := strconv.Atoi(pn)
+	pSize := ctx.DefaultQuery("psize", "10")
+	pSizeInt, _ := strconv.Atoi(pSize)
+
+	rsp, err := global.GoodsSrvClient.BrandList(context.Background(), &in.BrandFilterRequest{
+		Pages:       int32(pnInt),
+		PagePerNums: int32(pSizeInt),
+	})
+
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	result := make([]interface{}, 0)
+	reMap := make(map[string]interface{})
+	reMap["total"] = rsp.Total
+	for _, value := range rsp.Data[pnInt : pnInt*pSizeInt+pSizeInt] {
+		reMap := make(map[string]interface{})
+		reMap["id"] = value.Id
+		reMap["name"] = value.Name
+		reMap["logo"] = value.Logo
+
+		result = append(result, reMap)
+	}
+
+	reMap["data"] = result
+
+	ctx.JSON(http.StatusOK, reMap)
+}
+
+func NewBrand(ctx *gin.Context) {
+	brandForm := form.BrandForm{}
+	if err := ctx.ShouldBindJSON(&brandForm); err != nil {
+		api.HandleValidatorError(ctx, err)
+		return
+	}
+
+	rsp, err := global.GoodsSrvClient.CreateBrand(context.Background(), &in.BrandRequest{
+		Name: brandForm.Name,
+		Logo: brandForm.Logo,
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	request := make(map[string]interface{})
+	request["id"] = rsp.Id
+	request["name"] = rsp.Name
+	request["logo"] = rsp.Logo
+
+	ctx.JSON(http.StatusOK, request)
+}
+
+func DeleteBrand(ctx *gin.Context) {
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+	_, err = global.GoodsSrvClient.DeleteBrand(context.Background(), &in.BrandRequest{Id: int32(i)})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	ctx.Status(http.StatusOK)
+}
+
+func UpdateBrand(ctx *gin.Context) {
+	brandForm := form.BrandForm{}
+	if err := ctx.ShouldBindJSON(&brandForm); err != nil {
+		api.HandleValidatorError(ctx, err)
+		return
+	}
+
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	_, err = global.GoodsSrvClient.UpdateBrand(context.Background(), &in.BrandRequest{
+		Id:   int32(i),
+		Name: brandForm.Name,
+		Logo: brandForm.Logo,
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	ctx.Status(http.StatusOK)
+}
+
+func GetCategoryBrandList(ctx *gin.Context) {
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	rsp, err := global.GoodsSrvClient.GetCategoryBrandList(context.Background(), &in.CategoryInfoRequest{
+		Id: int32(i),
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		reMap := make(map[string]interface{})
+		reMap["id"] = value.Id
+		reMap["name"] = value.Name
+		reMap["logo"] = value.Logo
+
+		result = append(result, reMap)
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
+
+func CategoryBrandList(ctx *gin.Context) {
+	//所有的list返回的数据结构
+	/*
+		{
+			"total": 100,
+			"data":[{},{}]
+		}
+	*/
+	rsp, err := global.GoodsSrvClient.CategoryBrandList(context.Background(), &in.CategoryBrandFilterRequest{})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	reMap := map[string]interface{}{
+		"total": rsp.Total,
+	}
+
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		reMap := make(map[string]interface{})
+		reMap["id"] = value.Id
+		reMap["category"] = map[string]interface{}{
+			"id":   value.Category.Id,
+			"name": value.Category.Name,
+		}
+		reMap["brand"] = map[string]interface{}{
+			"id":   value.Brand.Id,
+			"name": value.Brand.Name,
+			"logo": value.Brand.Logo,
+		}
+
+		result = append(result, reMap)
+	}
+
+	reMap["data"] = result
+	ctx.JSON(http.StatusOK, reMap)
+}
+
+func NewCategoryBrand(ctx *gin.Context) {
+	categoryBrandForm := form.CategoryBrandForm{}
+	if err := ctx.ShouldBindJSON(&categoryBrandForm); err != nil {
+		api.HandleValidatorError(ctx, err)
+		return
+	}
+
+	rsp, err := global.GoodsSrvClient.CreateCategoryBrand(context.Background(), &in.CategoryBrandRequest{
+		CategoryId: int32(categoryBrandForm.CategoryId),
+		BrandId:    int32(categoryBrandForm.BrandId),
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	response := make(map[string]interface{})
+	response["id"] = rsp.Id
+
+	ctx.JSON(http.StatusOK, response)
+}
+
+func UpdateCategoryBrand(ctx *gin.Context) {
+	categoryBrandForm := form.CategoryBrandForm{}
+	if err := ctx.ShouldBindJSON(&categoryBrandForm); err != nil {
+		api.HandleValidatorError(ctx, err)
+		return
+	}
+
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	_, err = global.GoodsSrvClient.UpdateCategoryBrand(context.Background(), &in.CategoryBrandRequest{
+		Id:         int32(i),
+		CategoryId: int32(categoryBrandForm.CategoryId),
+		BrandId:    int32(categoryBrandForm.BrandId),
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	ctx.Status(http.StatusOK)
+}
+
+func DeleteCategoryBrand(ctx *gin.Context) {
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+	_, err = global.GoodsSrvClient.DeleteCategoryBrand(context.Background(), &in.CategoryBrandRequest{Id: int32(i)})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, "")
+}
--- web/goods_api/api/goods/goods.go
+++ web/goods_api/api/goods/goods.go
@@ -5,3 +5,33 @@
 **/
 
 package goods
+
+import "github.com/gin-gonic/gin"
+
+func List(c *gin.Context) {
+
+}
+
+func New(c *gin.Context) {
+
+}
+
+func Detail(c *gin.Context) {
+
+}
+
+func Delete(c *gin.Context) {
+
+}
+
+func Stocks(c *gin.Context) {
+
+}
+
+func UpdateStatus(c *gin.Context) {
+
+}
+
+func Update(c *gin.Context) {
+
+}
